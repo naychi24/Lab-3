1644a8ff7af9e910609d85f4c17230ce4c085ae5
Naychi <naychi.24@ichat.sp.edu.sg>
2025-05-26 20:54:31 +0800

    Lab3 PyTest modified

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,28 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_n_more_than_ten():
+    result = []
+    input_arr = [32, 35, 46, 78, 64, 34, 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == 1)
+
+def test_bubble_n_equal_zero():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == 0)
+
+def test_bubble_s_is_not_int():
+    result = []
+    input_arr = ["dog", 35, 46, 78, 64, 34, 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == 2)
+
